3484c36df57117883ce0dea53e77b3297de7e2db
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-08-17 22:23:20 +0530

    Books App

--- app/src/main/java/com/example/booksapplication/feature/presentation/Books/Books.kt
+++ app/src/main/java/com/example/booksapplication/feature/presentation/Books/Books.kt
@@ -5,12 +5,15 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
@@ -45,6 +48,20 @@ fun BookListScreen(viewModel: BooksViewModel = hiltViewModel()) {
                 }
                 Spacer(modifier = Modifier.padding(25.dp))
             }
+        }
+        if (state.error.isNotBlank()){
+            Text(
+                text = state.error,
+                color = MaterialTheme.colors.error,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)
+                    .align(Alignment.Center)
+            )
+        }
+        if (state.isLoading){
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
         }
     }
 }
