ef6ce0cd3d4a73d0ce2d9d68fd75dbaf36edabe2
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-08-19 06:04:22 +0530

    Books App

--- app/src/main/java/com/example/booksapplication/feature/data/repository/BooksRepositoryImpl.kt
+++ app/src/main/java/com/example/booksapplication/feature/data/repository/BooksRepositoryImpl.kt
@@ -2,7 +2,6 @@ package com.example.booksapplication.feature.data.repository
 
 import com.example.booksapplication.feature.data.remote.BooksApi
 import com.example.booksapplication.feature.data.remote.dto.BooksDto
-import com.example.booksapplication.feature.data.remote.dto.BooksDtoItem
 import com.example.booksapplication.feature.data.remote.dto.BooksSendingItem
 import com.example.booksapplication.feature.domain.repository.BooksRepository
 import javax.inject.Inject
